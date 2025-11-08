30c1ba0bffe6e31441c19d865628a0ac5fc0cced
AishwaryaPawar149 <149aishwaryapawar@gmail.com>
2025-11-08 23:05:54 +0530

    update

--- travelmemories-backend/src/main/java/com/aishwarya/travelmemories/TravelMemoriesApplication.java
+++
@@ -1,9 +0,0 @@
-package com.aishwarya.travelmemories;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class TravelMemoriesApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(TravelMemoriesApplication.class, args);
-    }
-}
--- travelmemories-backend/src/main/java/com/aishwarya/travelmemories/repository/MemoryRepository.java
+++
@@ -1,4 +0,0 @@
-package com.aishwarya.travelmemories.repository;
-import com.aishwarya.travelmemories.model.Memory;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface MemoryRepository extends JpaRepository<Memory, Long> {}
